01253a9bc3bc8c46c729080e28094af494d076f3
Fabrizio Perin (U114959) <fabrizio.perin@mobi.ch>
2017-05-03 14:17:30 +0200

    new scripts

---
+++ Assets/CustomScripts/Rampage/Code/Firescript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firescript : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit = new RaycastHit();
+        
+		if(Physics.Raycast(transform.position, Vector3.forward, out hit))
+        {
+            obj.transform.position = hit.transform.position;
+
+        }
+	}
+}
