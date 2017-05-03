c71c46c74b0498820b2fe9f173852c619cd64ac3
Utz Michel (U115726) <michel.utz@mobi.ch>
2017-05-03 17:00:48 +0200

    voice recognition

---
+++ Assets/CustomScripts/Interactions/code/CustomKeyWordManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class CustomKeyWordManager : MonoBehaviour, ISpeechHandler {
+    public void OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData)
+    {
+        SetPrice(eventData.RecognizedText);
+    }
+
+    public void SetPrice(string recognizedText)
+    {
+        Debug.Log("dfa");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
