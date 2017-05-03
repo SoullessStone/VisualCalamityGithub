068c8f106f27706430d08e7d4d676791bc3d4a6e
Utz Michel (U115726) <michel.utz@mobi.ch>
2017-05-03 15:58:45 +0200

    started to put notes in the scene

---
+++ Assets/CustomScripts/Interactions/code/InteractionsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class InteractionsManager : MonoBehaviour, IInputClickHandler
+{
+    public GameObject pointofInterest;
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("hey");
+        Instantiate(pointofInterest);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
