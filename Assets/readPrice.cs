ea2abe1d8bf5909896ef8644eb9375a53aa771a3
Hutchison John M. ( U111290 ) <john.hutchison@mobi.ch>
2017-05-04 16:12:24 +0200

    total price tweeks

--- Assets/readPrice.cs
+++ Assets/readPrice.cs
@@ -22,17 +22,17 @@ public class readPrice : MonoBehaviour {
         {
             if (priceDelta > 0)
             {
-                int delta = System.Math.Min(priceDelta, 13);
+                int delta = System.Math.Min(priceDelta, 97);
                 priceDelta -= delta;
                 price += delta;
             } else
             {
-                int delta = System.Math.Max(priceDelta, -13);
+                int delta = System.Math.Max(priceDelta, -97);
                 priceDelta += delta;
                 price -= delta;
             }
         }
-        text.text = price.ToString();
+        text.text = "CHF " + string.Format("{0:N0}", price) + ".-";
     }
 
     int calculatePrice()
