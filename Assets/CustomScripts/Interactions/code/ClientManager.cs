b7560d9e7bb8fbd3e5869cfd760f59031a5a1476
Mikhail <mikhail.chatillon@hotmail.com>
2017-05-04 18:27:26 +0200

    merged

---
+++ Assets/CustomScripts/Interactions/code/ClientManager.cs
@@ -0,0 +1,68 @@
+﻿using HoloToolkit.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA;
+
+public class ClientManager : MonoBehaviour
+{
+
+    public string ClientId = "18236782";
+    public int AnchorCounter = 0;
+    public GameObject Hologram;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Invoke("importAnchors", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void importAnchors()
+    {
+        if (WorldAnchorManager.Instance.AnchorStore != null)
+        {
+
+            String[] savedAnchorList = WorldAnchorManager.Instance.AnchorStore.GetAllIds();
+
+            foreach (string s in savedAnchorList)
+            {
+                if (s.StartsWith(ClientId))
+                {
+                    string tmp = s.Remove(0, 8);
+                    Debug.Log(tmp);
+                    int number = Int32.Parse(tmp);
+                    if (AnchorCounter < number)
+                    {
+                        AnchorCounter = number;
+                    }
+                    WorldAnchor world = WorldAnchorManager.Instance.AnchorStore.Load(s, Hologram);
+
+                    world.OnTrackingChanged += World_OnTrackingChanged;
+               
+                    GameObject go= GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    go.transform.position = world.transform.position;
+                    //WorldAnchorManager.Instance.AttachAnchor(Hologram, s);
+                }
+            }
+        }
+        else
+        {
+            Invoke("importAnchors", 5f);
+        }
+    }
+
+    private void World_OnTrackingChanged(WorldAnchor self, bool located)
+    {
+        // This simply activates/deactivates this object and all children when tracking changes
+        self.gameObject.SetActive(located);
+    }
+}
---
+++ Assets/CustomScripts/Interactions/code/TestScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Persistence;
+
+public class TestScript : MonoBehaviour {
+
+    public WorldAnchorStore AnchorStore { get; private set; }
+    public GameObject GameObject;
+    public string Client = "test";  
+    // Use this for initialization
+    void Start () {
+   
+        WorldAnchorStore.GetAsync(AnchorStoreReady);
+
+   
+    }
+
+    private void AnchorStoreReady(WorldAnchorStore store)
+    {
+        AnchorStore = store;
+        LoadGame();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void LoadGame()
+    {
+
+        // Save data about holograms positioned by this world anchor
+        AnchorStore.Load("test", gameObject);
+
+        string[] ids = AnchorStore.GetAllIds();
+        for (int index = 0; index < ids.Length; index++)
+        {
+            Debug.Log(ids[index]);
+        }
+    }
+}
