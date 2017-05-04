c34770367c9861e5b28c0585fd3fe13e6dcd6360
Hutchison John M. ( U111290 ) <john.hutchison@mobi.ch>
2017-05-04 10:49:17 +0200

    added dynamic price update, renamed HologramCollection

---
+++ Assets/readPrice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class readPrice : MonoBehaviour {
+
+    public int price = 1000;
+    public TextMesh text;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        price++;
+        if(price > 2000000)
+        {
+            price = 1000;
+        }
+        text.text = price.ToString();
+    }
+}
