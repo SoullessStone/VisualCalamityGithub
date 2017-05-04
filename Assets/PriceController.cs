8b38b3fd8a12c0a1f2172a9735f17a4b93e21f8a
Mikhail <mikhail.chatillon@hotmail.com>
2017-05-04 18:07:03 +0200

    changes to anchors

--- Assets/PriceController.cs
+++ Assets/PriceController.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PriceController : MonoBehaviour {
     public int Price = 1000;
-   
+    private ClientManager clientManager;
 
-	// Use this for initialization
-	void Start () {
-        GameObject PriceCollection = GameObject.Find("HologramCollection");
-     
-        this.transform.SetParent(PriceCollection.transform);
+    // Use this for initialization
+    void Start () {
+     //   GameObject PriceCollection = GameObject.Find("HologramCollection");
+       // this.transform.SetParent(PriceCollection.transform);
+
+        //Anchor Stuff
+        clientManager = GameObject.Find("ClientManager").GetComponent<ClientManager>();
+        clientManager.AnchorCounter++;
+        string tmp = String.Concat(clientManager.ClientId, clientManager.AnchorCounter.ToString());
+        WorldAnchorManager.Instance.AttachAnchor(this.gameObject,tmp);
 	}
 	
 	// Update is called once per frame
