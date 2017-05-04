8a2f705a8255d9f205480ef2f27ef05f33fe503c
Hutchison John M. ( U111290 ) <john.hutchison@mobi.ch>
2017-05-04 14:19:36 +0200

    added dynamic price calculation

--- Assets/readPrice.cs
+++ Assets/readPrice.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class readPrice : MonoBehaviour {
 
-    public int price = 1000;
+    public int price = 0;
+    int priceDelta = 0;
     public TextMesh text;
+    public GameObject collection;
 
     // Use this for initialization
     void Start() {
@@ -14,11 +16,38 @@ public class readPrice : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        price++;
-        if(price > 2000000)
+        int newPrice = calculatePrice();
+        priceDelta = newPrice - price;
+        if (priceDelta != 0)
         {
-            price = 1000;
+            if (priceDelta > 0)
+            {
+                int delta = System.Math.Min(priceDelta, 13);
+                priceDelta -= delta;
+                price += delta;
+            } else
+            {
+                int delta = System.Math.Max(priceDelta, -13);
+                priceDelta += delta;
+                price -= delta;
+            }
         }
         text.text = price.ToString();
+    }
+
+    int calculatePrice()
+    {
+        int newPrice = 0;
+
+        foreach (Transform child in collection.transform)
+        {
+            PriceController childPrice = child.GetComponent<PriceController>();
+            if(childPrice != null)
+            {
+                newPrice += childPrice.Price;
+            }
+        }
+
+        return newPrice;
     }
 }
