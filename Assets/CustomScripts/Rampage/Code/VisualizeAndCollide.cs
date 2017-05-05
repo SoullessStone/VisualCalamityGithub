8cc4956c01a9e0e8901746c4925250e63be3e17c
Fabrizio Perin (U114959) <fabrizio.perin@mobi.ch>
2017-05-05 11:53:29 +0200

    added flames

--- Assets/CustomScripts/Rampage/Code/VisualizeAndCollide.cs
+++ Assets/CustomScripts/Rampage/Code/VisualizeAndCollide.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class VisualizeAndCollide : MonoBehaviour {
 
     private ReadSchadenPrice readSchadenPrice;
+    public GameObject fireParticle;
 
     // Use this for initialization
     void Start () {
@@ -19,8 +20,11 @@ public class VisualizeAndCollide : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("PointOfInterest"))
         {
+            Instantiate(fireParticle,new Vector3(other.gameObject.transform.position.x, 
+                other.gameObject.transform.position.y, other.gameObject.transform.position.z),Quaternion.identity);
             other.gameObject.SetActive(false);
             readSchadenPrice.increaseRemovedItems();
+
         }
     }
 
