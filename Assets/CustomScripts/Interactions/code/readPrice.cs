1526b3e96f07b6a7150ea408c67f92d74db6e270
Hutchison John M. ( U111290 ) <john.hutchison@mobi.ch>
2017-05-05 12:05:17 +0200

    goldpated price animation

--- Assets/CustomScripts/Interactions/code/readPrice.cs
+++ Assets/CustomScripts/Interactions/code/readPrice.cs
@@ -2,37 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class readPrice : MonoBehaviour {
+public class readPrice : MonoBehaviour
+{
 
     public int price = 0;
     int priceDelta = 0;
+    int maxSteps = 30;
+    int steps = 0;
+    bool isNewPriceActive = false;
+    int numOfPrices = 0;
     public TextMesh text;
     public GameObject collection;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
+        text.text = "CHF " + string.Format("{0:N0}", 0) + ".-";
 
     }
 
     // Update is called once per frame
-    void Update() {
-        int newPrice = calculatePrice();
-        priceDelta = newPrice - price;
-        if (priceDelta != 0)
+    void Update()
+    {
         {
-            if (priceDelta > 0)
+            if (!isNewPriceActive)
             {
-                int delta = System.Math.Min(priceDelta, 97);
-                priceDelta -= delta;
-                price += delta;
-            } else
+                if (isNewPrice())
+                {
+                    isNewPriceActive = true;
+                    steps = 0;
+                }
+            }
+            else
             {
-                int delta = System.Math.Max(priceDelta, -97);
-                priceDelta += delta;
-                price -= delta;
+                if (steps > maxSteps)
+                {
+                    text.text = "CHF " + string.Format("{0:N0}", calculatePrice()) + ".-";
+                    isNewPriceActive = false;
+                    steps = 0;
+                }
+                steps++;
             }
         }
-        text.text = "CHF " + string.Format("{0:N0}", price) + ".-";
     }
 
     int calculatePrice()
@@ -42,7 +53,7 @@ public class readPrice : MonoBehaviour {
         foreach (Transform child in collection.transform)
         {
             PriceController childPrice = child.GetComponent<PriceController>();
-            if(childPrice != null)
+            if (childPrice != null)
             {
                 newPrice += childPrice.Price;
             }
@@ -50,4 +61,29 @@ public class readPrice : MonoBehaviour {
 
         return newPrice;
     }
+
+    bool isNewPrice()
+    {
+        int newNumOfPrices = 0;
+
+        foreach (Transform child in collection.transform)
+        {
+            PriceController childPrice = child.GetComponent<PriceController>();
+            if (childPrice != null)
+            {
+                newNumOfPrices++;
+            }
+        }
+
+        if (newNumOfPrices > numOfPrices)
+        {
+            numOfPrices = newNumOfPrices;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
