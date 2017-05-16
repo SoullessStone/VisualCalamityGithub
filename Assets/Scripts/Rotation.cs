675a86ad5d794fa9d866b0cfced05bb1cef62a02
Fabrizio Perin (U114959) <fabrizio.perin@mobi.ch>
2017-05-16 13:28:17 +0200

    added flipping coin

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Transform> ().Rotate (Vector3.up * rotationSpeed * Time.deltaTime);
+	}
+}
